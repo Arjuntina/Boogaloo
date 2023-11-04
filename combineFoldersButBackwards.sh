8639707476f474281440283fe5e1d509d84eb5ad
Arjun Patel <nujra911@gmail.com>
2023-11-04 09:27:35 -0400

    updated version

--- combineFoldersButBackwards.sh
+++ combineFoldersButBackwards.sh
@@ -6,8 +6,13 @@
 # Hopefully get to learn a bit more about bash/shell programming along the way
 
 
+# How to use:
+# Put all folders into one directory
+# Run the script!
+
+
 # Folder location with all the subfolders -- prompt user to enter it?
-FOLDERLOC="/home/arjuntina/Files/Reading/manga/FMA"
+FOLDERLOC="/home/arjuntina/Files/OnePiece"
 
 
 # Navigate to specified folder location and create "master" folder fo all the files to be copied into
@@ -20,7 +25,7 @@ I=0
 
 for FOLDER in *
 do
-	if [ ! "$FOLDER" = "master" ]
+	if [ ! "$FOLDER" = "master" ] && [-d "$FOLDER" ]
 	then
 		cd -- "$FOLDER"
 		for FILE in *
@@ -41,23 +46,24 @@ J=0
 # Loop over all folders in directory
 for FOLDER in *
 do
-	# If folder is not the master folder, then we navigate to it and copy all the files over.
-	if [ ! "$FOLDER" = "master" ]
-	then
-		cd -- "$FOLDER"
-		echo $FOLDER
-		# Loop through every file in each subdirectory
-		for FILE in *
-		do
-			# Name every folder as the index looped over & count the index down
-			mv "$FILE" "$I.jpg"
-			mv "$I.jpg" "$FOLDERLOC/master"
-			I=`expr $I - 1`
-		done
-		# move back to parent directory and iterate by 1
-		cd -- ..
-		J=`expr $J + 1`
-	fi
+    echo $FOLDER
+    # If folder is not the master folder, then we navigate to it and copy all the files over.
+    if [ ! "$FOLDER" = "master" ]
+    then
+        cd -- "$FOLDER"
+        echo $FOLDER
+        # Loop through every file in each subdirectory
+        for FILE in *
+        do
+            # Name every folder as the index looped over & count the index down
+            mv "$FILE" "$I.jpg"
+            mv "$I.jpg" "$FOLDERLOC/master"
+            I=`expr $I - 1`
+        done
+        # move back to parent directory and iterate by 1
+        cd -- ..
+        J=`expr $J + 1`
+    fi
 done
 
 # Displays the number of files & folders moved over to the user 
