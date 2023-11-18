c240fc39f033ec9a8e9e9c338441c739a9f67cda
Arjun Patel <nujra911@gmail.com>
2023-11-18 16:17:59 -0500

    updated scripts

--- combineFolders.sh
+++ combineFolders.sh
@@ -6,42 +6,66 @@
 # Hopefully get to learn a bit more about bash/shell programming along the way
 
 
+# How to use:
+# Put all folders into one directory
+# Run the script!
+
+
 # Folder location with all the subfolders -- prompt user to enter it?
-FOLDERLOC="/home/arjuntina/Files/Reading/manga/FMA"
+target_dir="/home/arjuntina/Files/OnePiece"
 
 
 # Navigate to specified folder location and create "master" folder fo all the files to be copied into
 (
-cd -- "$FOLDERLOC"
+cd -- "$target_dir"
 mkdir "master"
 
-# Counter for the nmber of files moved into "master" folder
-I=0
+# Counter for the number of files in the "master" folder
+i=0
 
-# Counter for the number of folders combined into a "master" folder
-J=0
-
-# Loop over all folders in directory
-for FOLDER in *
+for folder in *
 do
-	# If folder is not the master folder, then we navigate to it and copy all the files over.
-	if [ ! "$FOLDER" = "master" ]
+	if [ ! "$folder" = "master" ] && [-d "$folder" ]
 	then
-		cd -- "$FOLDER"
-		echo $FOLDER
-		# Loop through every file in each subdirectory
-		for FILE in *
+		cd -- "$folder"
+		for file in *
 		do
-			I=`expr $I + 1`
-			mv "$FILE" "$I.jpg"
-			mv "$I.jpg" "$FOLDERLOC/master"
+			i=`expr $i + 1`
 		done
-		# move back to parent directory and iterate by 1
-		cd -- ..
-		J=`expr $J + 1`
+		cd ..
 	fi
 done
 
+echo "$i files will be combined"
+
+k=$i
+
+# Counter for the number of folders combined into a "master" folder
+j=0
+
+# Loop over all folders in directory
+for folder in *
+do
+    echo $folder
+    # If folder is not the master folder, then we navigate to it and copy all the files over.
+    if [ ! "$folder" = "master" ]
+    then
+        cd -- "$folder"
+        echo $folder
+        # Loop through every file in each subdirectory
+        for file in *
+        do
+            # Name every folder as the index looped over & count the index down
+            mv "$file" "$i.jpg"
+            mv "$i.jpg" "$target_dir/master"
+            i=`expr $i - 1`
+        done
+        # move back to parent directory and iterate by 1
+        cd -- ..
+        j=`expr $j + 1`
+    fi
+done
+
 # Displays the number of files & folders moved over to the user 
-echo "$I files from $J folders copied and moved over"
+echo "$k files from $j folders copied and moved over"
 )
--- copyConfigFiles.sh
+++ copyConfigFiles.sh
@@ -1,1 +1,48 @@
 #!/bin/zsh
+
+# Folder location of config files
+config_folder_loc="/home/arjuntina/.config"
+
+# Target location of config files
+target_dir_loc="/home/arjuntina/Backups/programConfigFiles"
+
+# folders within config directory I DON'T want to back up
+# mainly because they take up space + not particularly useful
+# To-do: prompt users to review/potentially edit this during runtime? hmm
+folders_to_exclude=("discord" "Slack" "zoom" "zoom.us" "zoom.conf" "VSCodium" "chromium")
+
+# Subshell
+# Copy the config folder to the target location and systematically remove those listed in the folders_to_exclude array
+(
+# navigate to the target directory
+cd -- "$target_dir_loc"
+
+# remove existing config folder backup & copy current config folder backup to the new location
+rm -rf ".config"
+echo "Existing config folder removed"
+cp -rf "$config_folder_loc" "$target_dir_loc"
+echo -e "New config folder copied\n"
+
+# Enter into the config folder backup 
+cd ".config"
+
+echo "Removing unwanted folders & files..."
+for i in {1..${#folders_to_exclude[@]}}
+do
+    rm -rf "${folders_to_exclude[$i]}"
+    echo "removed ${folders_to_exclude[$i]}"
+done
+echo -e "Done\n"
+
+# Output the current size of the config directory
+cd ..
+echo "Current size of the .config backup"
+du -hs
+
+)
+
+# Unset the variables defined in the expression above?
+# Should already be unset but idk if this is good practice
+unset config_folder_loc
+unset target_dir_loc
+unset folders_to_exclude
